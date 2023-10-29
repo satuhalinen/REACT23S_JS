c3e69c7894d8d4193118b81f3e8b224d2eed583d
[satuhalinen] <satuh9@gmail.com>
2023-10-29 19:40:00 +0200

    getElementByIds replaced by querySelectorAll and HTML validation done

--- Margit's_lessons/Pizza_order_application/main.js
+++ Margit's_lessons/Pizza_order_application/main.js
@@ -1,84 +1,22 @@
 function calculator() {
-  let price;
+  let price = 0;
   let result;
   let circleResult;
-  let radio2 = document.getElementById("radio2");
-  let radio4 = document.getElementById("radio4");
-  let radio6 = document.getElementById("radio6");
-  let radio8 = document.getElementById("radio8");
-  let blueCheese = document.getElementById("blueCheese");
-  let cheese = document.getElementById("cheese");
-  let ham = document.getElementById("ham");
-  let mincedMeat = document.getElementById("mincedMeat");
-  let mushroom = document.getElementById("mushroom");
-  let olive = document.getElementById("olive");
-  let paprika = document.getElementById("paprika");
-  let pepperoni = document.getElementById("pepperoni");
-  let pineapple = document.getElementById("pineapple");
-  let redOnion = document.getElementById("redOnion");
-  let shrimp = document.getElementById("shrimp");
-  let tunaFish = document.getElementById("tunaFish");
+
   let deliveryOption = document.getElementById("deliveryOptions").value;
   let choices = 0;
 
-  if (radio2.checked) {
-    price = 7.5;
-  } else if (radio4.checked) {
-    price = 10.5;
-  } else if (radio6.checked) {
-    price = 12.5;
-  } else if (radio8.checked) {
-    price = 15.5;
-  } else {
-    price = 0;
+  for (const item of toppings) {
+    if (item.checked) {
+      choices += 1;
+    }
   }
 
-  if (blueCheese.checked) {
-    choices += 1;
-  }
-
-  if (cheese.checked) {
-    choices += 1;
-  }
-
-  if (ham.checked) {
-    choices += 1;
-  }
-
-  if (mincedMeat.checked) {
-    choices += 1;
-  }
-
-  if (mushroom.checked) {
-    choices += 1;
-  }
-
-  if (olive.checked) {
-    choices += 1;
-  }
-
-  if (paprika.checked) {
-    choices += 1;
-  }
-
-  if (pepperoni.checked) {
-    choices += 1;
-  }
-
-  if (pineapple.checked) {
-    choices += 1;
-  }
-
-  if (redOnion.checked) {
-    choices += 1;
-  }
-
-  if (shrimp.checked) {
-    choices += 1;
-  }
-
-  if (tunaFish.checked) {
-    choices += 1;
+  for (const item of radio) {
+    if (item.checked) {
+      price = item.value;
+      price = parseFloat(price);
+    }
   }
 
   if (choices > 4) {
@@ -96,22 +34,16 @@ function calculator() {
   console.log(price + " €");
   console.log(deliveryOption);
 }
-radio2.addEventListener("change", calculator);
-radio4.addEventListener("change", calculator);
-radio6.addEventListener("change", calculator);
-radio8.addEventListener("change", calculator);
 
 deliveryOptions.addEventListener("change", calculator);
 
-blueCheese.addEventListener("change", calculator);
-cheese.addEventListener("change", calculator);
-ham.addEventListener("change", calculator);
-mincedMeat.addEventListener("change", calculator);
-mushroom.addEventListener("change", calculator);
-olive.addEventListener("change", calculator);
-paprika.addEventListener("change", calculator);
-pepperoni.addEventListener("change", calculator);
-pineapple.addEventListener("change", calculator);
-redOnion.addEventListener("change", calculator);
-shrimp.addEventListener("change", calculator);
-tunaFish.addEventListener("change", calculator);
+let toppings = document.querySelectorAll(".toppings input");
+let radio = document.querySelectorAll(".size input");
+
+for (const item of toppings) {
+  item.addEventListener("change", calculator);
+}
+
+for (const item of radio) {
+  item.addEventListener("change", calculator);
+}
